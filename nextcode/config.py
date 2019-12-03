82c479be6902f05fd55539420ff65b4f28ce83e1
gzur <gzur@gzur.org>
2019-12-03 11:54:46 +0000

    GOP-786 - Remove unnecessary debug log

--- nextcode/config.py
+++ nextcode/config.py
@@ -110,8 +110,6 @@ def _load_config() -> Dict:
 def save_config() -> None:
     config = Config()
     config_filename = root_config_folder.joinpath("config.yaml")
-    no_profiles = len(config.get("profiles"))
-    log.debug("Saving config with %s profiles to %s", no_profiles, config_filename)
     os.makedirs(root_config_folder, exist_ok=True)
     with config_filename.open("w") as conf_file:
         yaml.safe_dump(config.dict(), conf_file)
