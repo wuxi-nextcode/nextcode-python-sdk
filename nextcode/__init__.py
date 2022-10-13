577ebeaf981f6d24c9a9daf5d607264b923b1560
Logi Leifsson <logi@genuitysci.com>
2022-10-13 11:25:39 +0000

    remove unneeded code

--- nextcode/__init__.py
+++ nextcode/__init__.py
@@ -18,13 +18,6 @@ log = logging.getLogger()
 cfg = Config()
 
 
-#def read_version():
-#    directory = os.path.dirname(__file__)
-#    with open(os.path.join(directory, "VERSION"), "r") as version_file:
-#        version = version_file.readline().strip()
-#        return version
-
-
 __version__ = importlib.metadata.version("nextcode-sdk")
 
 # loading this here allows easy extension setup in jupyterhub
